32ab5979202be1fc10fd26c795c3586093eb783c
Wee Jerrick <weejerrick@gmail.com>
2021-08-02 09:02:50 +0800

    refactor: 💡 Remove redundancies

--- controllers/browse.js
+++ controllers/browse.js
@@ -12,59 +12,7 @@ Step 3: Find Dogs whose sex is opposite of user's dog
 Step 4: Find Dogs whose breed is same as user's dog
 */
 
-const alreadyLiked = [
-  "6100f6f946ef971451c9a05c",
-  "6100f6f946ef971451c9a069",
-  "6100f6f946ef971451c9a06b",
-];
-
-const allSuggestions = [
-  "6100f6f946ef971451c9a05c",
-  "6100f6f946ef971451c9a069",
-  "6100f6f946ef971451c9a06b",
-  "6100f6f946ef971451c9a071",
-  "6100f6f946ef971451c9a072",
-  "6100f6f946ef971451c9a073",
-  "6100f6f946ef971451c9a074",
-  "6100f6f946ef971451c9a064",
-  "6100f6f946ef971451c9a05d",
-  "6100f6f946ef971451c9a05f",
-  "6100f6f946ef971451c9a061",
-];
-
-const answer = allSuggestions.filter((el) => !alreadyLiked.includes(el));
-console.log(answer.length);
-
-const filterDogs = (allSuggestions, alreadyLiked) => {
-  allSuggestions.filter((el) => !alreadyLiked.includes(el));
-};
-
 // Browse
-router.post("/archive", (req, res) => {
-  User.find({ location: { $in: req.body.userLocation } }, (err, foundUsers) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-    }
-    const ownerUsernames = foundUsers.map((user) => user.username);
-    Dog.find(
-      {
-        $and: [
-          { breed: req.body.dogBreed },
-          { sex: req.body.dogSex },
-          { ownerUsername: { $in: ownerUsernames } },
-        ],
-      },
-      (err, foundDogs) => {
-        if (err) {
-          res.status(400).json({ error: err.message });
-        }
-        res.status(200).json(foundDogs);
-      }
-    );
-  });
-});
-
-// New Test
 router.post("/", (req, res) => {
   User.find({ location: { $in: req.body.userLocation } }, (err, foundUsers) => {
     if (err) {
